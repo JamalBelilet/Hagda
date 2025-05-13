b9acab4fcee092e1b0a052eb68d7fbaf74c2c757
Djamaleddine Belilet <jamalbelilet@gmail.com>
2025-05-13 19:56:12 +0100

    Initial Commit

---
+++ Hagda/HagdaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HagdaApp.swift
+//  Hagda
+//
+//  Created by Djamaleddine Belilet on 13/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HagdaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HagdaTests/HagdaTests.swift
@@ -0,0 +1,16 @@
+//
+//  HagdaTests.swift
+//  HagdaTests
+//
+//  Created by Djamaleddine Belilet on 13/05/2025.
+//
+
+import Testing
+
+struct HagdaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
